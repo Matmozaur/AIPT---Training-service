45f50078b9894c9e29e37e3888c80742bd98404f
maciej.dobrzanski <matt950806@wp.pl>
2024-07-17 21:29:53 +0200

    Logging

--- app/main.py
+++ app/main.py
@@ -7,8 +7,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.endpoints_training import router_trainings
 
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s:%(lineno)d:  %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 
--- app/services/training_service.py
+++ app/services/training_service.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from models.exercises import Exercise, ExercisePerformed, TrainingExercisePerformedLink
@@ -19,33 +20,41 @@ class TrainingService:
 
     def analyse_training(self, training_input: TrainingInput) -> TrainingEvaluation:
         with Session(self.engine) as session:
+            logging.debug(training_input)
             training = Training()
-            session.add(training)
-            session.commit()  # Ensure training.id is populated
+            logging.debug(training)
+            # session.add(training)
+            # session.commit()
 
             total_exercises = 0
             total_sets = 0
 
             # Iterate over the input exercises
             for exercise_id, sets in training_input.exercises.items():
+                logging.debug(exercise_id)
+                logging.debug(sets)
                 # Ensure the exercise exists
                 exercise = session.query(Exercise).filter(Exercise.id == exercise_id).first()
+                logging.debug(exercise)
                 if not exercise:
                     raise ValueError(f"Exercise with id {exercise_id} does not exist")
 
                 # Create ExercisePerformed instance
                 exercise_performed = ExercisePerformed(sets=sets, exercise_id=exercise_id)
-                session.add(exercise_performed)
-                session.commit()  # Ensure exercise_performed.id is populated
+                logging.debug(exercise_performed)
+                # session.add(exercise_performed)
+                # session.commit()
 
                 # Link to Training
                 training_exercise_performed_link = TrainingExercisePerformedLink(
                     training_id=training.id,
                     exerciseperformed_id=exercise_performed.id
                 )
-                session.add(training_exercise_performed_link)
+                # session.add(training_exercise_performed_link)
 
                 total_exercises += 1
                 total_sets += sets
+                logging.debug(training_exercise_performed_link)
 
+            logging.debug(training)
             return TrainingEvaluation(effective_sets=effective_sets(training))
